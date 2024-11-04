89efd4b5a9e07e0f3ca4c3a4a2d20cb0ea3fa29e
syuhas <syuhas22@gmail.com>
2024-11-04 13:30:55 -0500

    added folders/checkenv/httpd

---
+++ deploy/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Fetch instance IP from Jenkins environment variable
+INSTANCE_PUBLIC_IP="${INSTANCE_PUBLIC_DNS}"
+REMOTE_USER="${REMOTE_USER}"
+
+if [ -z "$INSTANCE_PUBLIC_DNS" || -z "$REMOTE_USER"]; then
+    echo "Could not find DNS or user for SSH connection"
+    exit 1
+fi
+
+echo $REMOTE_USER
+
+echo $INSTANCE_PUBLIC_DNS
+
