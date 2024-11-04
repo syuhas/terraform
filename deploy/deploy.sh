0ae72e3425c968f8dc941a6a9f2bef7c3318240a
syuhas <syuhas22@gmail.com>
2024-11-04 14:25:33 -0500

    acccess workspace

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,17 +4,20 @@
 # INSTANCE_PUBLIC_IP="${INSTANCE_PUBLIC_DNS}"
 REMOTE_USER="${USER}"
 
+echo ${WORKSPACE}
+
 # if [ -z "$INSTANCE_PUBLIC_DNS" || -z "$REMOTE_USER"]; then
 #     echo "Could not find DNS or user for SSH connection"
 #     exit 1
 # fi
 
-if [ -z "$REMOTE_USER"]; then
-    echo "Could not find DNS or user for SSH connection"
-    exit 1
-fi
+# echo "Remote user: {$REMOTE_USER}"
+# echo "Instance DNS: {$INSTANCE_PUBLIC_DNS}"
 
-echo $REMOTE_USER
+# echo "Connecting to remote server via SSH..."
 
-# echo $INSTANCE_PUBLIC_DNS
+# # Copy the build artifact to the remote server
+# ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$INSTANCE_PUBLIC_DNS" << EOF
+#     sudo dnf install httpd -y
+
 
