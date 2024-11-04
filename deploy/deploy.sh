3884ee31b26771cf6d453ab48929d2574726e2d3
syuhas <syuhas22@gmail.com>
2024-11-04 14:11:44 -0500

    remote user again

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,11 +4,12 @@
 
 REMOTE_USER="${REMOTE_USER}"
 
-if ["$REMOTE_USER"]; then
+if [ -z "$REMOTE_USER"]; then
     echo "Could not find DNS or user for SSH connection"
     exit 1
 fi
 
 echo $REMOTE_USER
+
 
 
