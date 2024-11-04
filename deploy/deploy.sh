fffc011c72ea630ef0014bff0cbd625c3908f2ab
syuhas <syuhas22@gmail.com>
2024-11-04 14:20:45 -0500

    remote user name

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Fetch instance IP from Jenkins environment variable
+# INSTANCE_PUBLIC_IP="${INSTANCE_PUBLIC_DNS}"
+REMOTE_USER="${USER}"
 
-REMOTE_USER="${REMOTE_USER}"
+# if [ -z "$INSTANCE_PUBLIC_DNS" || -z "$REMOTE_USER"]; then
+#     echo "Could not find DNS or user for SSH connection"
+#     exit 1
+# fi
 
 if [ -z "$REMOTE_USER"]; then
     echo "Could not find DNS or user for SSH connection"
@@ -11,5 +16,5 @@ fi
 
 echo $REMOTE_USER
 
-
+# echo $INSTANCE_PUBLIC_DNS
 
