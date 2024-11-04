21e99956e0e527360f253df1a48ac9b1c4b70e7e
syuhas <syuhas22@gmail.com>
2024-11-04 11:44:29 -0500

    version

--- config/ec2.tf
+++ config/ec2.tf
@@ -1,14 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
+  version = "~> 3.0"
 }
 
 resource "aws_instance" "testinstance" {
