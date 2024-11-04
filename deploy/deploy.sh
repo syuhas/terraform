a5785153b720e7f3b6716374f68de46842e9a34b
syuhas <syuhas22@gmail.com>
2024-11-04 14:29:36 -0500

    deployment test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# Fetch instance IP from Jenkins environment variable
-# INSTANCE_PUBLIC_IP="${INSTANCE_PUBLIC_DNS}"
+
+
+INSTANCE_PUBLIC_IP="${INSTANCE_PUBLIC_DNS}"
 REMOTE_USER="${USER}"
 
-echo ${WORKSPACE}
+if [ -z "$INSTANCE_PUBLIC_DNS" || -z "$REMOTE_USER"]; then
+    echo "Could not find DNS or user for SSH connection"
+    exit 1
+fi
 
-# if [ -z "$INSTANCE_PUBLIC_DNS" || -z "$REMOTE_USER"]; then
-#     echo "Could not find DNS or user for SSH connection"
-#     exit 1
-# fi
+echo "Remote user: {$REMOTE_USER}"
+echo "Instance DNS: {$INSTANCE_PUBLIC_DNS}"
 
-# echo "Remote user: {$REMOTE_USER}"
-# echo "Instance DNS: {$INSTANCE_PUBLIC_DNS}"
+echo "Copying project files to remote server..."
+scp -o StrictHostKeyChecking=no -r ./src "$REMOTE_USER@$INSTANCE_PUBLIC_DNS:/tmp"
 
-# echo "Connecting to remote server via SSH..."
+echo "Connecting to remote server via SSH..."
 
-# # Copy the build artifact to the remote server
-# ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$INSTANCE_PUBLIC_DNS" << EOF
-#     sudo dnf install httpd -y
+# Copy the build artifact to the remote server
+ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$INSTANCE_PUBLIC_DNS" << EOF
+    sudo dnf install httpd -y
+    sudo cp -r /tmp/src /var/www/html
+    sudo systemctl start httpd
+    sudo systemctl enable httpd
+EOF
 
+echo "Deployment complete!"
+    
 
